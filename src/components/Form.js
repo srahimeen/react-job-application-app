cbbe3352165c5dd28f14cdca8014f1c506ae51de
DESKTOP-SOUFIN\Soufin Rahimeen <soufinrahimeen@gmail.com>
2018-06-21 20:18:06 -0400

    switched form to bootstrap

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,34 +1,23 @@
 import React from "react";
 
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Input from '@material-ui/core/Input';
-import Typography from '@material-ui/core/Typography';
-import FormLabel from '@material-ui/core/FormLabel';
-import Grid from '@material-ui/core/Grid';
-
 class Form extends React.Component {
 	render() {
 		return (
 			<div>
-				<Grid container spacing={8}>
-					<Grid item xs={12} sm={12}>
-						<form onSubmit={this.props.submitClickHandler} >
-							<TextField
-							name="firstName"
-							label="First Name"
-							margin="normal"
-							/> <span /> <br />
-							<TextField
-							name="lastName"
-							label="Last Name"
-							margin="normal"
-							/> <span /> <br />
-							<br />
-							<Button variant="raised" color="primary" type="submit">Submit</Button>
-						</form>
-					</Grid>
-				</Grid>
+				<form onSubmit={this.props.submitClickHandler}>
+					<div class="container">
+						<div className="form-group">
+							<div class="row">
+								<div class="col">
+									<label for="exampleInputEmail1">First Name</label>
+								</div>
+								<div class="col">
+									<input type="text" class="form-control" name="firstName" placeholder="First Name"></input>
+								</div>
+							</div>
+						</div>
+					</div>
+				</form>
 			</div>
 		);
 	}
