ba1a8991a3b018e7d1dd9e5b609195f1d33233e1
DESKTOP-SOUFIN\Soufin Rahimeen <soufinrahimeen@gmail.com>
2018-06-22 21:55:36 -0400

    dropdown working

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,60 +1,76 @@
 import React from "react";
 
-import Typography from '@material-ui/core/Typography';
-
-import { Container, Row, Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Container, Row, Col, Button, Form, FormGroup, Label, Input, FormText, Dropdown, DropdownToggle, DropdownMenu, DropdownItem  } from 'reactstrap';
 
 class Application extends React.Component {
+
+	//dropdown logic start
+	constructor(props) {
+		super(props);
+	
+		this.toggle = this.toggle.bind(this);
+		this.state = {
+		  dropdownOpen: false,
+		  stateSelected: "State"
+		};
+	  }
+	
+	toggle(event) {
+	this.setState(prevState => ({
+		dropdownOpen: !prevState.dropdownOpen,
+		stateSelected: event.currentTarget.textContent
+	}));
+	}
+
+	//dropdown logic end
+
 	render() {
 		return (
 			<div>
-				<form onSubmit={this.props.submitClickHandler}>
+				<Form onSubmit={this.props.submitClickHandler}>
 					<Container>
-						<div className="form-group">
+						<FormGroup>
 							<Row className="top-buffer">
 								<Col>
-									<Typography variant="subheading" htmlFor="firstName">First Name</Typography>
+									<Label variant="subheading" for="firstName">First Name</Label>
 								</Col>
 								<Col>
-									<input type="text" className="form-control" name="firstName" placeholder="First Name"></input>
+									<Input type="text" className="form-control" name="firstName" placeholder="First Name"></Input>
 								</Col>
 							</Row>
 							<Row className="top-buffer">
 								<Col>
-									<Typography variant="subheading" htmlFor="lastName">Last Name</Typography>
+									<Label variant="subheading" for="lastName">Last Name</Label>
 								</Col>
 								<Col>
-									<input type="text" className="form-control" name="lastName" placeholder="Last Name"></input>
+									<Input type="text" className="form-control" name="lastName" placeholder="Last Name"></Input>
 								</Col>
 							</Row>
 							<Row className="top-buffer">
 								<Col>
-									<Typography variant="subheading" htmlFor="state">State</Typography>
+									<Label variant="subheading" for="state">State</Label>
 								</Col>
 								<Col>
-									<div className="dropdown">
-										<button 
-											className="btn btn-secondary dropdown-toggle" 
-											type="button" 
-											id="dropdownMenuButton" 
-											data-toggle="dropdown" 
-											aria-haspopup="true">
-											Dropdown
-										</button>
-										<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-											<a className="dropdown-item" href="#nogo">Item 1</a>
-											<a className="dropdown-item" href="#nogo">Item 2</a>
-											<a className="dropdown-item" href="#nogo">Item 3</a>
-										</div>
-									</div>
+									<Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+										<DropdownToggle caret>
+											{this.state.stateSelected}
+										</DropdownToggle>
+										<DropdownMenu>
+											<DropdownItem>FL</DropdownItem>
+											<DropdownItem>CA</DropdownItem>
+											<DropdownItem>NY</DropdownItem>
+										</DropdownMenu>
+									</Dropdown>
 								</Col>
 							</Row>
-							<div className="row justify-content-center top-buffer">
-								<button type="submit" className="btn btn-primary">Submit</button>
-							</div>
-						</div>
+							<Row className="justify-content-center top-buffer">
+								<Col>
+									<Button type="submit" className="btn btn-secondary">Submit</Button>
+								</Col>
+							</Row>
+						</FormGroup>
 					</Container>
-				</form>
+				</Form>
 			</div>
 		);
 	}
